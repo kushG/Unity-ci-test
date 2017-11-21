92653f9dc6b99490a01f6aed3246cc2284484e36
Kushagra Gupta <kushagrag@sturfee.com>
2017-11-21 15:44:24 -0800

    Ads install.sh and build.sh

---
+++ Scripts/install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
+# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+echo 'Downloading from https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg?_ga=2.161908853.812206690.1511210911-100329522.1503524927: '
+curl -o Unity.pkg https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg?_ga=2.161908853.812206690.1511210911-100329522.1503524927
+
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
+
