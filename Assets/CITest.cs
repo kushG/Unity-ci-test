71231097125a03b96f37ba3bad03ad5dfa2fa36a
Kushagra Gupta <kushagrag@sturfee.com>
2017-11-21 14:09:07 -0800

    First commit

---
+++ Assets/CITest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class CITest {
+
+
+
+	[UnityTest]
+	public IEnumerator _Test_Pass() {
+		Assert.True (true);
+		yield return null;
+	}
+
+	[UnityTest]
+	public IEnumerator _Test_Fail() {
+		Assert.True (false);
+		yield return null;
+	}
+}
